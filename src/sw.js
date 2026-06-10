1c2f98d423525ff8fc88d5b6574c2c5283bb5986
celiamoransaras <celia.moransaras@gmail.com>
2026-06-10 09:50:42 +0200

    SW: limpiar todas las cachés al activar para forzar actualización en iOS

--- src/sw.js
+++ src/sw.js
@@ -5,7 +5,13 @@ precacheAndRoute(self.__WB_MANIFEST)
 
 // Tomar control inmediatamente al actualizar (sin esperar a cerrar tabs)
 self.addEventListener('install', () => self.skipWaiting())
-self.addEventListener('activate', (e) => e.waitUntil(self.clients.claim()))
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => caches.delete(key)))
+    ).then(() => self.clients.claim())
+  )
+})
 
 // ---- Push notifications ----
 self.addEventListener('push', (event) => {
